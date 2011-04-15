fc6fe2b0d31c4245a9c213fbe462db18bf2a07c6
Patrick Michael Chiuco <patrick.chiuco@gmail.com>
2011-04-15 10:31:24 +0800

    final setup. Not yet sure for media storage. Working on that now

--- engineeringorange/settings.py
+++ engineeringorange/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'orangeDB',                      # Or path to database file if using sqlite3.
         'USER': 'webteam',                      # Not used with sqlite3.
         'PASSWORD': 'pishIsnak',                  # Not used with sqlite3.
@@ -45,12 +45,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = 'media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
