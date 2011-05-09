ac2c6ae0b807548aa95f911818ba738024474baf
Patrick Chiuco <patrick.chiuco@gmail.com>
2011-05-09 14:35:19 +0800

    fixed initial urlconf for the admin site.

--- engineeringorange/urls.py
+++ engineeringorange/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('resume.views',
     # Examples:
@@ -14,8 +14,8 @@ urlpatterns = patterns('resume.views',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^media/',),
-    url(r'^$','index'),
+    #url(r'^media/',),
+    #url(r'^$','index'),
 )
 
 urlpatterns += patterns('',
